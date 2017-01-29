fdefd6d2a2610cd2fadf815c43dc8dd858bbab7d
Anthony Morast <anthonymorast33@hotmail.com>
2017-01-28 22:42:06 -0700

    a few more functions to help plot

--- project1/lc.r
+++ project1/lc.r
@@ -1,4 +1,4 @@
-run <- function(filename) {
+getGoodData <- function(filename) {
   lcData <- read.csv(filename)
   library(dplyr)
   
@@ -15,4 +15,9 @@ getGoodStanding <- function(df) {
 
 getDefault <- function(df) {
   return(filter(df, loan_status %in% c("Charged Off", "Default")))
+}
+
+intRateToNumeric <- function(df) {
+  df$int_rate <- sapply(df$int_rate, function(x) as.numeric(substr(x, 1, nchar(as.character(x))-1)))
+  return(df)
 }
